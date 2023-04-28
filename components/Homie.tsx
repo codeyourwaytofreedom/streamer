145ffa57b2dee0abf4d5ad607862dc37510a42ba
codeyourwaytofreedom <can.kolej@gmail.com>
2023-04-28 23:02:09 +0300

    Final time safely captured.
    Time catching bug solved.
    
    Finally some progress with the code.
    
    Keep going.
    
    Remmeber why you started.

--- components/Homie.tsx
+++ components/Homie.tsx
@@ -1,18 +1,70 @@
-import { useEffect, useRef } from "react";
+import { use, useEffect, useRef } from "react";
 import h from "../styles/Homie.module.css";
 import { useState } from "react";
-import ReactPlayer from 'react-player';
 
 
 
 
 const Homie = () => {
-
     const [videoUrl, setVideoUrl] = useState<string | null>(null);
     const [chunks, setChunks] = useState<Uint8Array[]>([]);
-    const start = 0;
-    const end = 1000000;
+    const [b_range,setRange] = useState({start:0, end:2000000})
     const videoRef = useRef<HTMLVideoElement>(null);
+    const chunker_button = useRef<HTMLButtonElement>(null);
+    const [cTime, setCurrentTime] = useState(0);
+
+    
+/*     useEffect(()=> {
+      console.log("initial")
+      const fetchData = async () => {
+        try {
+          const response = await fetch(
+            `http://localhost:3000/api/streamer`,
+            {
+              method: 'GET',
+              headers: { Range: `bytes=${b_range.start}-${b_range.end}`, file: 'boxing.mp4' },
+            }
+          );
+          const reader = response.body?.getReader();    
+          let reservoir:Uint8Array[]=[];  
+          const read = () => {
+            reader?.read().then(({ done, value }) => {
+              if (done) {
+                const videoBlob = new Blob(reservoir, { type: 'video/mp4' });
+                const videoUrl = URL.createObjectURL(videoBlob);
+                setVideoUrl(videoUrl);
+                setChunks(reservoir);
+                return;
+              }
+              reservoir.push(value);
+              read();
+            });
+          };
+          read();
+        } catch (error) {
+          console.error(error);
+        }
+      };
+      fetchData();
+      setRange({start:b_range.end, end:b_range.end+200000});
+    },[]); */
+    
+    const [last, setLast] = useState(0);
+
+    useEffect(()=>{
+      if(videoRef.current){
+        
+        if(cTime-videoRef.current.currentTime === 0){
+          console.log("this is the spot I am looking for");
+          console.log(last)
+        }
+        else{
+          console.log("good technique...aha");
+          console.log(cTime);
+          setLast(cTime)
+        }
+      }
+    },[cTime])
 
     const add = () => {
         const fetchData = async () => {
@@ -21,24 +73,22 @@ const Homie = () => {
                 `http://localhost:3000/api/streamer`,
                 {
                   method: 'GET',
-                  headers: { Range: `bytes=${start}-${end}`, file: 'boxing.mp4' },
+                  headers: { Range: `bytes=${b_range.start}-${b_range.end}`, file: 'boxing.mp4' },
                 }
               );
-              const reader = response.body?.getReader();
-              const receivedChunks: Uint8Array[] = [];
-      
+              const reader = response.body?.getReader();   
+              let reservoir:Uint8Array[]=[];    
+              chunks.map(c=> reservoir.push(c));
               const read = () => {
                 reader?.read().then(({ done, value }) => {
                   if (done) {
-                    const videoBlob = new Blob(chunks, { type: 'video/mp4' });
+                    const videoBlob = new Blob(reservoir, { type: 'video/mp4' });
                     const videoUrl = URL.createObjectURL(videoBlob);
                     setVideoUrl(videoUrl);
-                    console.log("one",receivedChunks);
-                    console.log("two",chunks);
+                    setChunks(reservoir);
                     return;
                   }
-                  receivedChunks.push(value);
-                  chunks.push(value)
+                  reservoir.push(value)
                   read();
                 });
               };
@@ -48,55 +98,26 @@ const Homie = () => {
             }
           };
           fetchData();
+          setRange({start:b_range.end, end:b_range.end+200000});
     }
-
-    const add2 = () => {
-      const fetchData = async () => {
-          try {
-            const response = await fetch(
-              `http://localhost:3000/api/streamer`,
-              {
-                method: 'GET',
-                headers: { Range: `bytes=${end}-${end+200000}`, file: 'boxing.mp4' },
-              }
-            );
-            const reader = response.body?.getReader();
-            const receivedChunks: Uint8Array[] = [];
-    
-            const read = () => {
-              reader?.read().then(({ done, value }) => {
-                if (done) {
-                  const videoBlob = new Blob(chunks, { type: 'video/mp4' });
-                  const videoUrl = URL.createObjectURL(videoBlob);
-                  setVideoUrl(videoUrl);
-                  console.log("one",receivedChunks);
-                  console.log("two",chunks);
-                  return;
-                }
-                receivedChunks.push(value);
-                chunks.push(value)
-                read();
-              });
-            };
-            read();
-          } catch (error) {
-            console.error(error);
-          }
-        };
-        fetchData();
-  }
 
     return ( 
         <>
             <>
             <div className={h.homie}>
                 <div>
-                    <button onClick={add}>PART ONE</button>
-                    <button onClick={add2}>PART TWO</button>
+                    <button onClick={add} ref={chunker_button}>PART ONE</button>
+                    <h1>Chunks : {chunks.length}</h1>
+                    <h1>Last Time: {last.toFixed(2)}</h1>
                     <h1>{videoUrl}</h1>
+                    <h1>{b_range.start} - {b_range.end}</h1>
                     {
                         videoUrl &&
-                        <video src={videoUrl} ref={videoRef} controls />
+                        <video src={videoUrl} ref={videoRef} 
+                        onTimeUpdate={()=> setCurrentTime(videoRef.current!.currentTime)}
+                        controls
+                        autoPlay
+                         />
 
                     }
                 </div>
