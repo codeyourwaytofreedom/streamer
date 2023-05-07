1f1bd7d22cf278ea78132bf37e9aa584182f8be8
codeyourwaytofreedom <can.kolej@gmail.com>
2023-05-07 13:14:44 +0300

    Back to media source extention.
    Will count on Safari for now.
    
    Keep going.
    
    Remmeber why you started.

--- components/Homie.tsx
+++ components/Homie.tsx
@@ -1,24 +1,45 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
 const Homie = () => {
   const videoRef = useRef<HTMLVideoElement>(null);
-    let bytesReceived = 0;
+  const [ready, setReady] = useState(false);
+
+
   useEffect(() => {
     const mimeCodec = 'video/mp4; codecs="avc1.42E01E, mp4a.40.2"';
-    //const mimeCodec = 'video/mp4; codecs="avc1.64001F, mp4a.40.2"';
+    let bytesReceived = 0;
 
-    const response = fetch(`http://localhost:3000/api/streamer`, {
-      headers: { Range: "bytes=0-500", file:"bunny.mp4" }, // Fetch the first chunk of the video
-    })
+    if ("MediaSource" in window && MediaSource.isTypeSupported(mimeCodec)) {
+      const mediaSource = new MediaSource();
+      videoRef.current!.src = URL.createObjectURL(mediaSource);
+      mediaSource.addEventListener("sourceopen", async () => {
+        const sourceBuffer = mediaSource.addSourceBuffer(mimeCodec);
+        const response = await fetch(`http://localhost:3000/api/streamer`, {
+          headers: { Range: "bytes=0-3999999", file:"she.mp4" }, // Fetch the first chunk of the video
+        });
+        const arrayBuffer = await response.arrayBuffer();
+        sourceBuffer.appendBuffer(arrayBuffer);
+/*         bytesReceived += arrayBuffer.byteLength;
+
+        setInterval(async () => {
+          const start = bytesReceived;
+          const end = bytesReceived + 1000000 - 1;
+          const response = await fetch(`http://localhost:3000/api/streamer`, {
+            headers: { Range: `bytes=${start}-${end}`, file:"tiger.mp4" },
+          });
+          const arrayBuffer = await response.arrayBuffer();
+          sourceBuffer.appendBuffer(arrayBuffer);
+          bytesReceived += arrayBuffer.byteLength;
+        }, 2000); */
+      });
+    } else {
+      console.error("Unsupported MIME type or codec: ", mimeCodec);
+    }
   }, []);
 
-  return <>
-    <video ref={videoRef} controls autoPlay>
-      <source type={"video/mp4"} src={"/api/streamer"} />
-    </video>
-  </>
+  return <video ref={videoRef} controls />;
 }
- 
+
 export default Homie;
 
 /*         bytesReceived += arrayBuffer.byteLength;
--- pages/api/streamer.ts
+++ pages/api/streamer.ts
@@ -3,40 +3,38 @@ import { NextApiRequest, NextApiResponse } from 'next'
 import fs from 'fs';
 import { createReadStream } from 'fs';
 import path from 'path';
-import ffmpeg from 'fluent-ffmpeg';
-import ffprobeStatic from 'ffprobe-static';
 
-ffmpeg.setFfprobePath(ffprobeStatic.path);
 
-export  default function handler(
+export default function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  console.log(req.headers.file)
-  const videoPath = path.join(process.cwd(), 'excluded', "boxing.mp4");
+
+  console.log(req.headers.file);
+  const videoPath = path.join(process.cwd(), "excluded", `${req.headers.file}`);
   const file_details = fs.statSync(videoPath);
   const fileSize = file_details.size;
   const byte_range = req.headers.range;
 
-  if(byte_range){
-    console.log("byte range received",byte_range);
-    const slice = byte_range.replace(/bytes=/, '').split('-');
-    console.log(slice)
+  if (byte_range) {
+    console.log("byte range received", byte_range);
+    const slice = byte_range.replace(/bytes=/, "").split("-");
+    console.log(slice);
 
     const start = parseInt(slice[0], 10);
     const end = slice[1] ? parseInt(slice[1], 10) : fileSize - 1;
     const chunksize = end - start;
-    const file = createReadStream(videoPath, ({ start, end }));
+    const file = createReadStream(videoPath, { start, end });
     const head = {
-      'Content-Range': `bytes ${start}-${end}/${fileSize}`,
-      'Accept-Ranges': 'bytes',
-      'Content-Length': chunksize,
-      'Content-Type': 'video/mp4',
+      "Content-Range": `bytes ${start}-${end}/${fileSize}`,
+      "Accept-Ranges": "bytes",
+      "Content-Length": chunksize,
+      "Content-Type": "video/mp4",
     };
     res.writeHead(206, head);
     file.pipe(res);
   }
-/*   else{
+/*     else{
     const head = {
         'Content-Length': fileSize,
         //'Content-Type': 'video/mp4; codecs="avc1.42E01E, mp4a.40.2"',
@@ -45,6 +43,7 @@ export  default function handler(
     fs.createReadStream(videoPath).pipe(res);
   } */
 }
+
 
 
 
