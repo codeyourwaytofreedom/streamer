8a820b69c41db4b182faef0f16288872f5dfd776
codeyourwaytofreedom <can.kolej@gmail.com>
2023-05-14 22:54:49 +0300

    Thumbnail image successfully created.
    Converted into blob and displayed on clientside.
    
    Next step: repear the process for all videos.
    
    Keep going.
    
    Remember why you started.

--- components/Fights.tsx
+++ components/Fights.tsx
@@ -1,19 +1,27 @@
 import h from "../styles/Homie.module.css";
-import { MouseEvent, useEffect } from "react";
+import { MouseEvent, useEffect, useRef, useState } from "react";
+import Image from "next/image";
 
 interface Vid_Chan_Prop {
     handle_video_change: (e: MouseEvent<HTMLButtonElement>) => void;
 }
 
 const Fights_row = ({handle_video_change}:Vid_Chan_Prop) => {
+    const [test, setTest] = useState<string>();
+    const o = useRef(null);
+
     useEffect(()=>{
-        fetch("http://localhost:3000/api/thumbnails").then(res => res.text()).then(tx => console.log(tx))
+        fetch("http://localhost:3000/api/thumbnails")
+        .then(res => res.blob()).then(b => URL.createObjectURL(b)).then(r => setTest(r))
     },[])
     return ( 
         <div className={h.homie_fights}>
             <button value={"boxing.mp4"} onClick={e=>handle_video_change(e)}>One</button>
             <button value={"bunny.mp4"} onClick={e=>handle_video_change(e)}>Two</button>
             <button value={"she.mp4"} onClick={e=>handle_video_change(e)}>Three</button>
+            {
+                test && <Image ref={o}  src={test} alt={"test"} width={300} height={300} /> 
+            }
         </div>
      );
 }
--- pages/api/thumbnails.ts
+++ pages/api/thumbnails.ts
@@ -1,6 +1,7 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import path from 'path';
 import Ffmpeg from 'fluent-ffmpeg';
+import fs from 'fs';
 
 async function generateThumbnail(videoPath: string, thumbnailPath: string): Promise<void> {
     return new Promise<void>((resolve, reject) => {
@@ -26,11 +27,19 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-    const vPath = path.join(process.cwd(), "excluded", "she.mp4");
+    const vPath = path.join(process.cwd(), "excluded", "bunny.mp4");
     const thumbnailPath = path.join(process.cwd(), "excluded", "thumbnail.jpg");
     try {
         await generateThumbnail(vPath, thumbnailPath);
         console.log('Thumbnail generated successfully');
+
+        // Read the thumbnail image file contents
+        const thumbnailData = fs.readFileSync(thumbnailPath);
+
+        // Set the response headers and send the file contents
+        res.setHeader('Content-Type', 'image/jpeg');
+        res.send(thumbnailData);
+
         res.send(thumbnailPath);
       } catch (error) {
         console.error('Error generating thumbnail:', error);
