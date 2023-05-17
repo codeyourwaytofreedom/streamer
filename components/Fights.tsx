ba1d8c38e78a28e84ddd301e8c10d4dafcf3cf81
codeyourwaytofreedom <can.kolej@gmail.com>
2023-05-17 18:25:25 +0300

    Responsive in place.
    2 seperate menu sections.
    
    Next step: build menu content.
    
    Keep going.
    
    Remember why you started.

--- components/Fights.tsx
+++ components/Fights.tsx
@@ -15,21 +15,19 @@ const Fights_row = () => {
 
     return ( 
         <div className={h.homie_fights}>
-{/*             <button value={"boxing.mp4"} onClick={e=>handle_video_change(e)}>One</button>
-            <button value={"bunny.mp4"} onClick={e=>handle_video_change(e)}>Two</button>
-            <button value={"she.mp4"} onClick={e=>handle_video_change(e)}>Three</button> */}
-
             {  images.length !== 0 && images.map((image,index) => (
-                <Link href={`/watch/${image.name.split(".")[0]}.mp4`} key={index}>
-                    <Image
-                    src={`data:image/jpeg;base64,${image.data}`}
-                    alt={image.name}
-                    width={400}
-                    height={300}
-                    />
-                </Link>
-                ))}
-
+              <div className={h.homie_fights_each}>
+                  <Link href={`/watch/${image.name.split(".")[0]}.mp4`} key={index}>
+                      <Image
+                      src={`data:image/jpeg;base64,${image.data}`}
+                      alt={image.name}
+                      width={400}
+                      height={300}
+                      />
+                  </Link>
+              </div>
+            ))}
+          <div className={h.homie_fights_column}>Hello</div>
         </div>
      );
 }
